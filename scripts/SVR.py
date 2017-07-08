49d5509788ed9bdec248e18b49da24612584afdc
Rosie Zou <y53zou@uwaterloo.ca>
2017-07-08 18:08:32 -0400

    added sys.stdout.flush

--- scripts/SVR.py
+++ scripts/SVR.py
@@ -1,17 +1,14 @@
 # Curve Smoothing with Support Vector Regression using Radial Basis Function (RBF) kernel
-# Written by Rosie Zou, June 2017
-
-# syntax for calling the support vector regression function
-#     SVR(kernel='rbf', degree=3, gamma='auto', coef0=0.0, tol=0.001,
-#         C=1.0, epsilon=0.1, shrinking=True, cache_size=200, verbose=False, max_iter=-1)
+# SVR(kernel='rbf', degree=3, gamma='auto', coef0=0.0, tol=0.001,
+#     C=1.0, epsilon=0.1, shrinking=True, cache_size=200, verbose=False, max_iter=-1)
 
 # Sample data input format:
 # {"time":1009,"sensor":"gyro","data":[-1.54875,12.19750,11.36625]}
 import numpy as np
 from sklearn.svm import SVR
 import timeit
-import json
 import matplotlib.pyplot as plt
+import sys
 
 start = timeit.default_timer()
 
@@ -29,7 +26,7 @@ def SVR_process_monotype(JSONArray):
         z.append(JSONArray[count]["data"][2])
         t.append([JSONArray[count]["time"]])
 
-    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1, verbose=False)
+    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.2, verbose=False)
     x = np.array(x)
     y = np.array(y)
     z = np.array(z)
@@ -45,19 +42,19 @@ def SVR_process_monotype(JSONArray):
     # red for x, blue for y and green for z
     plt.plot(t, x_rbf, 'r', t, y_rbf, 'b', t, z_rbf, 'g', t, x, 'r--', t, y, 'bs', t, z, 'g^', lw = 2)
     plt.show()
-
-    return returnJSONArray
+    print(returnJSONArray)
+    sys.stdout.flush()
 
 stop = timeit.default_timer()
 
 print(stop - start)
 
 
-## DEMO
+# DEMO
 
-DemoData = []
-for i in range(50):
-    i += 1
-    DemoData.append({"time": i, "sensor": "gyro", "data": [np.random.uniform(0, i), np.random.chisquare(i), np.random.binomial(50, (float)(i/(i+1)))]})
-
-SVR_process_monotype(DemoData)
+# DemoData = []
+# for i in range(50):
+#     i += 1
+#     DemoData.append({"time": i, "sensor": "gyro", "data": [np.random.uniform(0, i), np.random.chisquare(i), np.random.binomial(50, (float)(i/(i+1)))]})
+#
+# SVR_process_monotype(DemoData)
